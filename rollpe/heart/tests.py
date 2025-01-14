ef228a502d8596d799438d77df3587eb691c45a9
chanung-ki <117616574+chanung-ki@users.noreply.github.com>
2025-01-15 00:19:54 +0900

    refactor: heart 작성 테스트코드 리팩토링

--- rollpe/heart/tests.py
+++ rollpe/heart/tests.py
@@ -23,10 +23,12 @@ from heart.models import Heart
     - 비공개 롤링 페이퍼 =>
         3. 로그인 o, 초대된 유저 x => 조회 불가능
         4. 로그인 o, 초대된 유저 o => 조회 가능
+        
+        
+    - 인덱스 , 블러처리 필요
 
 """
 class HeartReadAPITest(APITestCase):
-    
     @classmethod
     def create_test_users(cls):
         """테스트 유저 생성"""
@@ -255,11 +257,10 @@ class HeartReadAPITest(APITestCase):
         
     
 
-
 class HeartCreateAPITest(APITestCase):
     @classmethod
-    def setUpTestData(cls):
-        # 테스트 유저 생성
+    def create_test_users(cls):
+        """테스트 유저 생성"""
         cls.receiver = User.objects.create(
             name='receiver',
             email='receiver@gmail.com',
@@ -272,82 +273,130 @@ class HeartCreateAPITest(APITestCase):
             name='test_user1',
             email='testuser1@gmail.com',
         )
+        cls.user2 = User.objects.create(
+            name='test_user2',
+            email='testuser2@gmail.com',
+        )
         cls.user1.set_password('1234')
         cls.user1.save()
-        
-        # 테스트 롤링페이퍼 생성
-        cls.rolling_paper = Paper.objects.create(
+        cls.user2.set_password('1234')
+        cls.user2.save()
+
+    @classmethod
+    def create_test_data(cls):
+        """테스트 롤링페이퍼와 마음 생성"""
+        # 테스트 롤링페이퍼 생성 (공개)
+        cls.public_rolling_paper = Paper.objects.create(
             receiverFK=cls.receiver,
             hostFK=cls.host,
             receivingDate='2025-01-10',
-            title='테스트 롤링페이퍼',
+            title='공개 롤링페이퍼',
             description='테스트 입니다.',
-            password='1234'
+            viewStat=True,
         )
         
-        cls.heart_data = {
-            'userFK': cls.user1.id,
-            'paperFK': cls.rolling_paper.id,
-            'context': '테스트 입니다.' 
-        }
+        # 테스트 롤링페이퍼 생성 (비공개)
+        cls.private_rolling_paper = Paper.objects.create(
+            receiverFK=cls.receiver,
+            hostFK=cls.host,
+            receivingDate='2025-01-10',
+            title='비공개 롤링페이퍼',
+            description='테스트 입니다.',
+            password='1234',
+            viewStat=False,
+        )
         
+        # 롤링페이퍼로 유저 초대
+        cls.private_rolling_paper.invitingUser.add(cls.user1)
+        cls.private_rolling_paper.save()
+        
+
+    @classmethod
+    def setUpTestData(cls):
+        """테스트 데이터 생성"""
+        cls.create_test_users()
+        cls.create_test_data()
         cls.heart_url = reverse('heart_api')
-    
         
+        cls.public_heart_data = {
+            'paperFK': cls.public_rolling_paper.id,
+            'context': '마음1 테스트 입니다.',
+            'index': 1
+        }
+        cls.private_heart_data = {
+            'paperFK': cls.private_rolling_paper.id,
+            'context': '마음2 테스트 입니다.',
+            'index': 1
+        }
     
-    def signin(self):
+    def signin(self, user):
         """
             테스트유저 로그인
         """
         signin_url = reverse('token_obtain_pair')
-        signin_data = {
-            'email': 'testuser1@gmail.com',
-            'password': '1234',
-        }
+        signin_data = {}
+        
+        if user == 'user1':
+            signin_data['email'] = self.user1.email
+            signin_data['password'] = '1234'
+        else:
+            signin_data['email'] = self.user2.email
+            signin_data['password'] = '1234'
+            
         response = self.client.post(signin_url, signin_data)
         token = response.data.get('data')['access']
+        
         # 인증 헤더 설정
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
-    
     
         
     def test_create_heart_without_authenticated(self):
         """
             로그인하지 않은 유저는 마음을 작성할 수 없다.
         """
-        response = self.client.post(self.heart_url, self.heart_data, format='json')
+        response = self.client.post(self.heart_url, self.public_heart_data, format='json')
         
         # 응답 상태 코드 확인
         self.assertEqual(response.status_code, 401, '상태코드가 올바르지 않습니다.')
-        
         
         
     def test_create_heart_without_invited(self):
         """
             초대 받지 않은 유저는 마음을 작성할 수 없다.
         """
-        pass
-    
+        self.signin('user2')
+        response = self.client.post(self.heart_url, self.private_heart_data, format='json')
+        
+        # 응답 상태 코드 확인
+        self.assertEqual(response.status_code, 471, '상태코드가 올바르지 않습니다.')
     
     
     def test_create_heart(self):
         """
             새로운 마음을 생성할 수 있어야 한다.
         """
-        self.signin()
-        response = self.client.post(self.heart_url, self.heart_data, format='json')
+        self.signin('user1')
+        private_response = self.client.post(self.heart_url, self.private_heart_data, format='json')
+        public_response = self.client.post(self.heart_url, self.public_heart_data, format='json')
         
         # 응답 상태 코드 확인
-        self.assertEqual(response.status_code, 201, '상태코드가 올바르지 않습니다.')
+        self.assertEqual(private_response.status_code, 201, '상태코드가 올바르지 않습니다.')
         
+        # 응답 상태 코드 확인
+        self.assertEqual(public_response.status_code, 201, '상태코드가 올바르지 않습니다.')
     
     
     def test_create_heart_duplicate(self):
         """
             이미 마음을 작성한 유저는 재작성할 수 없다.
         """
-        pass
+        self.signin('user1')
+        self.client.post(self.heart_url, self.private_heart_data, format='json')
+        
+        # 재작성 요청
+        response = self.client.post(self.heart_url, self.private_heart_data, format='json')
     
+        self.assertEqual(response.status_code, 482, '상태코드가 올바르지 않습니다.')
     
     
     
