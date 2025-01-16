1dcd6fa724542c6d9c8daa8534fc7628947111e7
Jeongmingz <joungminyt@gmail.com>
2025-01-16 15:32:43 +0900

    feat: Custom Response 변경사항 1. 공통으로 사용되는 Response Code의 message 내용을 변경 가능하도록 설정. 2. set_cookie를 사용 가능하도록 변경

--- rollpe/utils/response.py
+++ rollpe/utils/response.py
@@ -1,16 +1,15 @@
-from rest_framework.response import Response
+from django.http import JsonResponse
 from rest_framework import status
 
-# 예시: SystemCodeManager는 프로젝트 특성에 맞춰 자유롭게 작성/수정
 from utils.constants import SystemCodeManager
 
 
-class CustomResponse(Response):
+class CustomResponse(JsonResponse):
 	def __init__(self, data=None, **kwargs):
 		http_status = kwargs.pop("status", status.HTTP_200_OK)
 
 		code = SystemCodeManager.get_code(http_status)
-		msg = SystemCodeManager.get_message(http_status)
+		msg = kwargs.pop("msg", SystemCodeManager.get_message(http_status))
 		link = kwargs.pop("link", SystemCodeManager.get_link(http_status))
 
 		# 최종 payload의 형태를 원하는 대로 구성
@@ -22,12 +21,16 @@ class CustomResponse(Response):
 			"data": data,
 			}
 
-		# Response의 초기화는 최종 payload와 kwargs를 넘겨줍니다.
 		super().__init__(payload, status=http_status, **kwargs)
 
+	def set_cookie(self, key, value, **options):
+		"""
+		Wrapper for setting a cookie on the response.
+		"""
+		self.cookies[key] = value
+		for option_key, option_value in options.items():
+			setattr(self.cookies[key], option_key, option_value)
+
+
 def Response(**kwargs):
-	"""
-	DRF의 Response가 아닌 custom Response를 간단하게 호출하기 위한 함수.
-	return Response(data=...) 형태로 사용할 수 있도록 만든다.
-	"""
 	return CustomResponse(**kwargs)
